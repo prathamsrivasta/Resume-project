f0603402900197e027e85d6c1d7feab32775ae4b
prathamsrivasta <prathamsrivastava92@gmail.com>
2025-02-28 00:01:05 +0530

    first commit

---
+++ ai-resume/src/Home/index.jsx
@@ -0,0 +1,15 @@
+import Header from '@/components/custum/Header'
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+    Landing page
+     {/* <UserButton/> */}
+    </div>
+  )
+}
+
+export default Home
