27c518cee616639fe5ebab0619e3bd1959b31cb7
prathamsrivasta <prathamsrivastava92@gmail.com>
2026-01-21 22:54:01 +0530

    done

--- ai-resume/src/dashboard/index.jsx
+++ ai-resume/src/dashboard/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import AddResume from "./components/AddResume";
 import { useUser } from "@clerk/clerk-react";
 import GlobalApi from "./../../service/GlobalApi";
-import ResumeCardItem from "./components/ResumeCardItem";
+import ResumeCardItem from "./components/ResumeCardItem.jsx";
 
 function Dashboard() {
   const { user } = useUser();
